6f1e0061dce86c139250f2285567783601959c33
Dennis Ziemer <37363429+holmescodes@users.noreply.github.com>
2021-01-08 11:23:13 +0100

    Update replaceWithAlphabetPosition.js

--- Code Wars/6 kyu/replaceWithAlphabetPosition.js
+++ Code Wars/6 kyu/replaceWithAlphabetPosition.js
@@ -20,4 +20,4 @@ function alphabetPosition(text){
     return numbers.join(" ");
 }
 
-console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
