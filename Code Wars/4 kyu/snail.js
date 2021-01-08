e170ffe7187d33cba507721443045f7b02686697
Dennis Ziemer <37363429+holmescodes@users.noreply.github.com>
2021-01-08 11:56:06 +0100

    Update snail.js

--- Code Wars/4 kyu/snail.js
+++ Code Wars/4 kyu/snail.js
@@ -29,9 +29,20 @@ function snail(arr) {
     for (let k = arr.length - 1; k > 0; k--) {
       way.push(arr[k].shift());
     }
-    console.log(arr);
   }
   return way;
 }
 
-console.log(snail([[1]]));
+let array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(snail(array)); // [1,2,3,6,9,8,7,4,5]
+
+array = [
+  [1, 2, 3],
+  [8, 9, 4],
+  [7, 6, 5],
+];
+console.log(snail(array)); // [1,2,3,4,5,6,7,8,9]
