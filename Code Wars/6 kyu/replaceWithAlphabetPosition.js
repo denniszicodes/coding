94d9a2aad2f1d61cc4186e0662769e5492c074ec
Dennis Ziemer <37363429+holmescodes@users.noreply.github.com>
2021-01-08 11:29:06 +0100

    Update replaceWithAlphabetPosition.js

--- Code Wars/6 kyu/replaceWithAlphabetPosition.js
+++ Code Wars/6 kyu/replaceWithAlphabetPosition.js
@@ -8,6 +8,12 @@
 
 // "a" = 1, "b" = 2, etc.
 
+// Example
+
+// alphabetPosition("The sunset sets at twelve o' clock.")
+
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
 function alphabetPosition(text){
     let numbers = [];
     for(const char of text.toLowerCase()){
